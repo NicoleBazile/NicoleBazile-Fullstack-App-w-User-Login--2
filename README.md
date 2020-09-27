55bc5dba0039b62e2da09bb17303515d189eb11b
Nicole B <63973798+NicoleBazile@users.noreply.github.com>
2020-09-26 20:18:41 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Fullstack-App-w-User-Login-2
+Created an a Tennis League Leaderboard application with admin login and sign up capabilities. Also attached the an external database to store the inputted data of each of the users and messages board data
+
+**Link to project:**
+
+<!-- ![GitHub Logo](/img/logotest.png) -->
+
+## How It's Made:
+**Tech used:** HTML/CSS,JavaScript, Node.js, Express.js, MongoDB
+
+
+## Optimizations
+pending..
+
+## Lessons Learned:
+Developed a better understanding of how passports for log ins set ups work.
---
+++ app/routes.js
@@ -0,0 +1,118 @@
+module.exports = function(app, passport, db) {
+
+// normal routes ===============================================================
+
+    // show the home page (will also have our login links)
+    app.get('/', function(req, res) {
+        res.render('index.ejs');
+    });
+
+    // PROFILE SECTION =========================
+    app.get('/profile', isLoggedIn, function(req, res) { //once you're login w a passport you'll get all the stuff about the user that is being login
+        db.collection('score').find().toArray((err, result) => {
+          if (err) return console.log(err)
+          result.sort(function(a, b){return b.wins - a.wins})
+          res.render('profile.ejs', {
+            user : req.user,
+            messages: result
+          })
+        })
+    });
+
+    // LOGOUT ==============================
+    app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+
+// message board routes ===============================================================
+
+    app.post('/messages', (req, res) => {
+      db.collection('score').save({name: req.body.name, team: req.body.team, wins: 0}, (err, result) => {
+        if (err) return console.log(err)
+        console.log('saved to database')
+        res.redirect('/profile')
+      })
+    })
+
+    app.put('/messages', (req, res) => {
+      db.collection('score')
+      .findOneAndUpdate({name: req.body.name, team: req.body.team}, {
+        $set: {
+          wins:req.body.wins + 1
+        }
+      }, {
+        sort: {_id: -1},
+        upsert: true
+      }, (err, result) => {
+        if (err) return res.send(err)
+        res.send(result)
+      })
+    })
+
+
+    app.delete('/messages', (req, res) => {
+      db.collection('score').findOneAndDelete({name: req.body.name, team: req.body.team}, (err, result) => {
+        if (err) return res.send(500, err)
+        res.send('Player deleted!')
+      })
+    })
+
+
+// =============================================================================
+// AUTHENTICATE (FIRST LOGIN) ==================================================
+// =============================================================================
+
+    // locally --------------------------------
+        // LOGIN ===============================
+        // show the login form
+        app.get('/login', function(req, res) {
+            res.render('login.ejs', { message: req.flash('loginMessage') });
+        });
+
+        // process the login form
+        app.post('/login', passport.authenticate('local-login', {
+            successRedirect : '/profile', // redirect to the secure profile section
+            failureRedirect : '/login', // redirect back to the signup page if there is an error
+            failureFlash : true // allow flash messages
+        }));
+
+        // SIGNUP =================================
+        // show the signup form
+        app.get('/signup', function(req, res) {
+            res.render('signup.ejs', { message: req.flash('signupMessage') });
+        });
+
+        // process the signup form
+        app.post('/signup', passport.authenticate('local-signup', {
+            successRedirect : '/profile', // redirect to the secure profile section
+            failureRedirect : '/signup', // redirect back to the signup page if there is an error
+            failureFlash : true // allow flash messages
+        }));
+
+// =============================================================================
+// UNLINK ACCOUNTS =============================================================
+// =============================================================================
+// used to unlink accounts. for social accounts, just remove the token
+// for local account, remove email and password
+// user account will stay active in case they want to reconnect in the future
+
+    // local -----------------------------------
+    app.get('/unlink/local', isLoggedIn, function(req, res) {
+        var user            = req.user;
+        user.local.email    = undefined;
+        user.local.password = undefined;
+        user.save(function(err) {
+            res.redirect('/profile');
+        });
+    });
+
+};
+
+// route middleware to ensure user is logged in
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated())
+        return next();
+
+    res.redirect('/');
+}
